f35978e636f5529f6540f7fefd84389cb7caa936
fhusnia <husniafeyzullayeva@gmail.com>
2022-11-19 12:32:19 +0400

    fc

---
+++ lesson1/app.py
@@ -0,0 +1,26 @@
+#2 pilus 10 vurulsun 3 çıxılsın 12 qüvvətdə 5 nəticəsini tapın
+result = ((2 + 10) * 3 - 12);
+print(result ** 5);
+#Verilmiş ədədin 3-ə bölünüb bölünmədiyini göstərən kod yazın. (hərhansı bir ədəd yaza bilərsiniz)
+x = int(input("reqemi daxil et: "))
+
+if x % 3 == 0:
+    print('reqem 3 e bolunur')
+else:
+    print("reqem 3 e bolunmur")
+
+#I don't need "capuccino" or something else bu yazını print edin
+print("""I don't need "capuccino" or something else""")
+#Python sözündə z hərfinin olmadığını təsdiqləyən kod yazın
+print('Python' in 'z');
+#ha yazısını 200 dəfə ekrana yazan kod hazırlayın
+print('ha' * 200);
+#Aşağıdakı stringləri birləşdirərək yeni bir cümlə yaradıb onu print edin
+evezlik = 'Men'
+isim = "Python"
+feil = "oyrenirem"
+bosluq = ' '
+print(evezlik + bosluq + isim  + bosluq + feil);
+#6-nın 3-ə bölünməsindən çıxan nəticənin tipini göstərin
+num = 6 / 3
+print(type(num));
